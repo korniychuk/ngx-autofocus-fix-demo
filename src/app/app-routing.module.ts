20bff1771648cf005cdc091837f18b72e267074f
korniychuk <dev@korniychuk.pro>
2019-08-24 04:30:36 +0300

    Initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '1-simple-example',
+    loadChildren: './1-simple-example/simple-example.module#SimpleExampleModule',
+  },
+  {
+    path: '2-simple-example-binding',
+    loadChildren: './2-simple-example-binding/simple-example-binding.module#SimpleExampleBindingModule',
+  },
+  {
+    path: '3-simple-example-smart',
+    loadChildren: './3-simple-example-smart/simple-example-smart.module#SimpleExampleSmartModule',
+  },
+  {
+    path: '4-angular-material',
+    loadChildren: './4-angular-material/angular-material.module#AngularMaterialModule',
+  },
+  {
+    path: '5-global-configuration',
+    loadChildren: './5-global-configuration/global-configuration.module#GlobalConfigurationModule',
+  },
+  {
+    path: '6-infinite-scroll',
+    loadChildren: './6-infinite-scroll/infinite-scroll.module#InfiniteScrollModule',
+  },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes),
+  ],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
---
+++ src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  imports:      [ BrowserModule, FormsModule, AppRoutingModule ],
+  declarations: [ AppComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
